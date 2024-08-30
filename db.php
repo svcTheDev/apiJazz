cb9c72150aaab512fe444d19fd05b37aea4d8a1a
Sergio Vargas <svargas.ext.uber@gmail.com>
2024-08-29 20:33:13 -0500

    adding env variables

--- db.php
+++ db.php
@@ -1,8 +1,15 @@
 <?php 
 
+    $DB_HOST=$_ENV['DB_HOST'];
+    $DB_USER=$_ENV['DB_USER'];
+    $DB_PASSWORD=$_ENV['DB_PASSWORD'];
+    $DB_NAME=$_ENV['DB_NAME'];
+    $DB_PORT=$_ENV['DB_PORT'];
+
     $db = dbConnection();
     function dbConnection() : mysqli {
-        $db = mysqli_connect('localhost', 'root', '', 'todo2', '3307');
+        $db = mysqli_connect($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME, $DB_PORT);
+        // $db = mysqli_connect('localhost', 'root', '', 'todo2', '3307');
 
         return $db;
     }
